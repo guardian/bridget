b62917c272625823e9187384ec13f08f4c03c04c
Jamie Webb <jamie.webb@guardian.co.uk>
2019-12-19 14:49:25 +0000

    thrift files with ad function and version number functions

---
+++ native.thrift
@@ -0,0 +1,5 @@
+service Native {
+    void insertAdverts(1:i32 x1, 2:i32 y1, 3:i32 w1, 4:i32 h1, 5:i32 x2, 6:i32 y2, 7:i32 w2, 8:i32 h2),
+
+    i32 webviewVersionNumber(),
+}
---
+++ webview.thrift
@@ -0,0 +1,3 @@
+service Webview {
+    i32 nativeVersionNumber(),
+}

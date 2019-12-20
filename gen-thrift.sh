1c4bee9d37d63ace6e9738228cf907c1fda70e11
Jamie Webb <jamie.webb@guardian.co.uk>
2019-12-20 12:49:26 +0000

    generate typescript files

---
+++ gen-thrift.sh
@@ -0,0 +1,15 @@
+# generate TypeScript Native Thrift files 
+node_modules/.bin/thrift-typescript --target thrift-server --rootDir . --sourceDir thrift --outDir TypeScript ../native.thrift
+
+# generate Swift Webview Thrift files 
+
+# generate Kotlin Webview Thrift files 
+
+# publish TypeScript package to npm
+
+# publish Swift cocoapod
+
+# publish Kotlin package to Maven?
+
+
+

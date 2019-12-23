49fcb5a66b3cc3e734f3922749af0224d26dce29
Jamie Webb <jamie.webb@guardian.co.uk>
2019-12-23 14:57:55 +0000

    update gen typescript script

--- gen-typescript.sh
+++ gen-typescript.sh
@@ -2,7 +2,10 @@
 node_modules/.bin/thrift-typescript --target thrift-server --rootDir . --sourceDir thrift --outDir mobile-apps-thrift-typescript ../thrift/native.thrift
 
 # publish TypeScript package to npm
-cd mobile-apps-thrift-typescript && npm init -y && npm publish --access public
+cd mobile-apps-thrift-typescript
+echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+npm init -y
+npm publish --access public
 
 
 

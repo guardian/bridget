3e7d67ec800a509f8dadd381e2859b212416988b
Jamie Webb <jamie.webb@guardian.co.uk>
2019-12-23 09:11:53 +0000

    Update native.thrift

--- native.thrift
+++ native.thrift
@@ -1,5 +1,12 @@
 service Native {
-    void insertAdverts(1:i32 x1, 2:i32 y1, 3:i32 w1, 4:i32 h1, 5:i32 x2, 6:i32 y2, 7:i32 w2, 8:i32 h2),
+
+    struct AdSlot {
+        1: optional i32 x;
+        2: optional i32 y;
+        3: optional i32 height;
+    }
+    
+    void insertAdverts(1:list<AdSlot> adSlots),
 
     i32 webviewVersionNumber(),
 }

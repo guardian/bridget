f560435409b19ea82b2b841d2eb0fbee4db98594
Jamie Webb <jamie.webb@guardian.co.uk>
2019-12-23 16:11:48 +0000

    bump tag version

--- gen-typescript.sh
+++ gen-typescript.sh
@@ -5,20 +5,4 @@ node_modules/.bin/thrift-typescript --target thrift-server --rootDir . --sourceD
 cd mobile-apps-thrift-typescript
 npm init -y
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
-npm version minor
-npm version minor
-npm version minor
-npm version minor
-npm version minor
-npm version minor
 npm publish --access public
-
-# update git with latest version
-git config --global user.email "GuardianAndroid"
-git config --global user.name "GuardianAndroid"
-
-git clone https://github.com/guardian/mobile-apps-thrift.git
-cd mobile-apps-thrift
-git add package.json
-git commit -m "increase version number"
-git push origin master

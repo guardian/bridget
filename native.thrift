353b7e40e6bc52849382fb3facaa7f9e2c2eae93
Jamie Webb <jamie.webb@guardian.co.uk>
2019-12-23 09:14:11 +0000

    Update native.thrift

--- native.thrift
+++ native.thrift
@@ -1,11 +1,10 @@
-service Native {
+struct AdSlot {
+    1: optional i32 x;
+    2: optional i32 y;
+    3: optional i32 height;
+}
 
-    struct AdSlot {
-        1: optional i32 x;
-        2: optional i32 y;
-        3: optional i32 height;
-    }
-    
+service Native {
     void insertAdverts(1:list<AdSlot> adSlots),
 
     i32 webviewVersionNumber(),

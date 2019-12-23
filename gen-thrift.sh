efbe04581a895702c54650259dd71b1a5f50af4c
Jamie Webb <jamie.webb@guardian.co.uk>
2019-12-23 09:40:20 +0000

    publish to npm

--- gen-thrift.sh
+++ gen-thrift.sh
@@ -1,11 +1,12 @@
 # generate TypeScript Native Thrift files 
-node_modules/.bin/thrift-typescript --target thrift-server --rootDir . --sourceDir thrift --outDir TypeScript ../native.thrift
+node_modules/.bin/thrift-typescript --target thrift-server --rootDir . --sourceDir thrift --outDir mobile-apps-thrift-typescript ../native.thrift
 
 # generate Swift Webview Thrift files 
 
 # generate Kotlin Webview Thrift files 
 
 # publish TypeScript package to npm
+cd mobile-apps-thrift-typescript && npm init -y && npm publish --access public
 
 # publish Swift cocoapod
 

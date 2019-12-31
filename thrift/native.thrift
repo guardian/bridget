1fa7faee40b74b884a97481a1031e494dd72058f
Jamie Webb <jamie.webb@guardian.co.uk>
2019-12-31 11:42:33 +0000

    add width param to adSlot

--- thrift/native.thrift
+++ thrift/native.thrift
@@ -2,10 +2,10 @@ struct AdSlot {
     1: optional i32 x;
     2: optional i32 y;
     3: optional i32 height;
+    4: optional i32 width;
 }
 
 service Native {
     void insertAdverts(1:list<AdSlot> adSlots),
     i32 webviewVersionNumber(),
-    i32 test(),
 }

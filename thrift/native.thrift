fe0dbed99317ab324fb6c6e30c1e010c90ca3768
Jamie Webb <jamie.webb@guardian.co.uk>
2020-01-30 15:52:00 +0000

    topic functions

--- thrift/native.thrift
+++ thrift/native.thrift
@@ -5,7 +5,14 @@ struct AdSlot {
     4: optional i32 width;
 }
 
+struct Topic {
+	1: required string id;
+}
+
 service Native {
     void insertAdverts(1:list<AdSlot> adSlots),
     i32 nativeThriftPackageVersion(),
+    void follow(topic: Topic),
+    void unfollow(topic: Topic),
+    bool isFollowing(topic: Topic),
 }

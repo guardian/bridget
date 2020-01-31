32787f6a69eaa6e77d854854e20f62c93d1e5725
Jamie Webb <jamie.webb@guardian.co.uk>
2020-01-31 10:27:27 +0000

    launch slideshow

--- thrift/native.thrift
+++ thrift/native.thrift
@@ -5,7 +5,12 @@ struct AdSlot {
     4: optional i32 width;
 }
 
+struct Image {
+    1: required string url;
+}
+
 service Native {
     void insertAdverts(1:list<AdSlot> adSlots),
     i32 nativeThriftPackageVersion(),
+    void launchSlideshow(1:list<Image> images),
 }

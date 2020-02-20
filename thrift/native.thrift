cd5d4fbe5f5cdad9e1bb115409b755774762b258
Max Spencer <mrmaxspencer@googlemail.com>
2020-02-20 16:43:50 +0000

    Fix indents

--- thrift/native.thrift
+++ thrift/native.thrift
@@ -16,10 +16,10 @@ struct Image {
 }
 
 struct Epic {
-	1: required string title;
-	2: required string body;
-	3: required string firstButton;
-	4: optional string secondButton;
+    1: required string title;
+    2: required string body;
+    3: required string firstButton;
+    4: optional string secondButton;
 }
 
 struct MaybeEpic {

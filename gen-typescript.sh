82547ad1319e429ec42bc94c05fd9462e275d900
Jamie Webb <jamie.webb@guardian.co.uk>
2020-03-06 13:34:27 +0000

    rename to bridget-typescript

--- gen-typescript.sh
+++ gen-typescript.sh
@@ -1,9 +1,9 @@
 # generate TypeScript Native Thrift files 
-node_modules/.bin/thrift-typescript --target thrift-server --rootDir . --sourceDir thrift --outDir mobile-apps-thrift-typescript ../thrift/native.thrift
-node_modules/.bin/thrift-typescript --target thrift-server --rootDir . --sourceDir thrift --outDir mobile-apps-thrift-typescript ../thrift/webview.thrift
+node_modules/.bin/thrift-typescript --target thrift-server --rootDir . --sourceDir thrift --outDir bridget-typescript ../thrift/native.thrift
+node_modules/.bin/thrift-typescript --target thrift-server --rootDir . --sourceDir thrift --outDir bridget-typescript ../thrift/webview.thrift
 
 # publish TypeScript package to npm
-cd mobile-apps-thrift-typescript
+cd bridget-typescript
 npm init -y
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
 
--- thrift/webview.thrift
+++ thrift/webview.thrift
@@ -1,4 +1,5 @@
 service Webview {
 	i32 webviewThriftPackage(),
 	void updateFontSize(1:i32 size),
+	void test(),
 }

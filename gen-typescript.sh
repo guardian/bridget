02fd3e30d5f173ce940e6e2a4d6986379f496f16
Jamie Webb <jamie.webb@guardian.co.uk>
2020-03-30 11:50:19 +0100

    generate and publish javascript files

--- gen-typescript.sh
+++ gen-typescript.sh
@@ -1,14 +1,23 @@
-# generate TypeScript Native Thrift files 
+# generate TypeScript Thrift files
 node_modules/.bin/thrift-typescript --target thrift-server --rootDir . --sourceDir thrift --outDir bridget-typescript ../thrift/native.thrift
 node_modules/.bin/thrift-typescript --target thrift-server --rootDir . --sourceDir thrift --outDir bridget-typescript ../thrift/webview.thrift
 
-# publish TypeScript package to npm
 cd bridget-typescript
+
+# create package.json
 npm init -y
 echo "//registry.npmjs.org/:_authToken=$NPM_TOKEN" >> ~/.npmrc
+npx json -I -f package.json -e 'this.types="index.d.ts"'
 
+# generate JavaScript files
+npm i typescript -g
+npx tsc --init --declaration
+npx tsc
+
+# use repo tags for version
 git fetch --all
 CURRENT_FULL_VERSION="$(git describe --tags --abbrev=0)"
 
+# publish to npm
 npm version ${CURRENT_FULL_VERSION}
 npm publish --access public

8948d9df4f387edc8762331c7d25bc163cac67d5
Jamie Webb <jamie.webb@guardian.co.uk>
2020-11-16 17:20:11 +0000

    stop reading unused parameter

--- .github/actions/validate-thrift/validate.sh
+++ .github/actions/validate-thrift/validate.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-set -e
 
 # Read arguments
 export ACCESS_TOKEN=$1
-export PLATFORM=$2
 
 # Git setup
 git config --global credential.helper "/bin/bash /credential-helper.sh"
 git config --global user.email '<>'
 
 git clone https://github.com/guardian/bridget.git
+
+cat bridget/native/native.thrift
 
 # Validate Thrift
 if thrift --gen swift:async_servers -r -out ./ bridget/thrift/native.thrift; then

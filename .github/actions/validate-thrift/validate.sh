db0868227668b9ea027f8e6ce4d7dec510e5b706
Jamie Webb <jamie.webb@guardian.co.uk>
2020-11-16 17:33:56 +0000

    remove unused github access credentials

--- .github/actions/validate-thrift/validate.sh
+++ .github/actions/validate-thrift/validate.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# Read arguments
-export ACCESS_TOKEN=$1
-
-# Git setup
-git config --global credential.helper "/bin/bash /credential-helper.sh"
-git config --global user.email '<>'
-
-git clone https://github.com/guardian/bridget.git
-
-cat bridget/native/native.thrift
-
-# Validate Thrift
 if thrift --gen swift:async_servers -r -out ./ bridget/thrift/native.thrift; then
     printf 'Validated Thrift ✅ proceeding to generate packages'
     exit 0

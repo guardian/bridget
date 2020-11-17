7ec91772c545bc76ee75638ad01bcd84d4bc97d7
Jamie Webb <jamie.webb@guardian.co.uk>
2020-11-17 10:53:45 +0000

    use github credentials

--- .github/actions/validate-thrift/validate.sh
+++ .github/actions/validate-thrift/validate.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+# Read arguments
+export ACCESS_TOKEN=$1
+
+# Git setup
+git config --global credential.helper "/bin/bash /credential-helper.sh"
+git config --global user.email '<>'
+
 git clone https://github.com/guardian/bridget.git
 
 cat bridget/native/native.thrift
 
-thrift --gen swift:async_servers -r -out ./ bridget/thrift/native.thrift
-
-exit 1
+# Validate Thrift
+if thrift --gen swift:async_servers -r -out ./ bridget/thrift/native.thrift; then
+    printf 'Validated Thrift ✅ proceeding to generate packages'
+    exit 0
+else
+    printf 'Invalid Thrift in native.thrift ❌'
+    exit 1
+fi

5ed0cfca4b1dd5a134ac940d63891f8edad3fb0b
Jamie Webb <jamie.webb@guardian.co.uk>
2020-11-17 10:44:06 +0000

    test validation

--- .github/actions/validate-thrift/validate.sh
+++ .github/actions/validate-thrift/validate.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-set -e
-
 git clone https://github.com/guardian/bridget.git
 
+cat bridget/native/native.thrift
+
 thrift --gen swift:async_servers -r -out ./ bridget/thrift/native.thrift
+
+exit 1

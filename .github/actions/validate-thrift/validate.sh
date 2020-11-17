a1a42357f2a25bf76d22e408875474253f14dc69
Jamie Webb <jamie.webb@guardian.co.uk>
2020-11-17 11:32:19 +0000

    use branch rather than main

--- .github/actions/validate-thrift/validate.sh
+++ .github/actions/validate-thrift/validate.sh
@@ -2,14 +2,16 @@
 
 # Read arguments
 export ACCESS_TOKEN=$1
+export BRANCH=$2
 
 # Git setup
 git config --global credential.helper "/bin/bash /credential-helper.sh"
 git config --global user.email '<>'
 
 git clone https://github.com/guardian/bridget.git
+git checkout ${BRANCH}
 
-cat bridget/native/native.thrift
+cat bridget/thrift/native.thrift
 
 # Validate Thrift
 if thrift --gen swift:async_servers -r -out ./ bridget/thrift/native.thrift; then
